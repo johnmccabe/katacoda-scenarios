070ef8905c98bdcbdbd4afeab02add56aa1839b8
John McCabe <john@johnmccabe.net>
2019-10-29 23:56:15 +0000

    Fix preload and run as nonroot

--- bsides-opa-2019/gatekeeper_1.0/assets/setup/preload-images.sh
+++ bsides-opa-2019/gatekeeper_1.0/assets/setup/preload-images.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
 # Preload docker images used in the workshop (run this first so Katacoda has time to provision Kube)
-declare -a StringArray=("openpolicyagent/opa:0.14.2-rootless" "openpolicyagent/kube-mgmt:0.9" "docker.io/kubernetes/pause" "gcr.io/google_containers/pause-amd64:3.0"
-"Windows Server 2016" )
-echo -n "Cache Images"
+declare -a StringArray=("openpolicyagent/opa:0.14.2-rootless" "openpolicyagent/kube-mgmt:0.9" "docker.io/kubernetes/pause" "gcr.io/google_containers/pause-amd64:3.0")
+echo "Cache Images"
 for image in ${StringArray[@]}; do
     docker pull ${image} 1>/dev/null
-    echo -n "- ${image}"
+    echo "- ${image}"
 done
 
 # Wait for Kubernetes to be up
