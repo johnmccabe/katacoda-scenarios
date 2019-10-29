35095e6ed6053331803bd00fd90cb8ca29eb1911
John McCabe <john@johnmccabe.net>
2019-10-29 21:40:32 +0000

    Update and move preload-images.sh

---
+++ bsides-opa-2019/gatekeeper_1.0/assets/setup/preload-images.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Preload docker images used in the workshop (run this first so Katacoda has time to provision Kube)
+declare -a StringArray=("openpolicyagent/opa:0.14.2-rootless" "openpolicyagent/kube-mgmt:0.9" "docker.io/kubernetes/pause" "gcr.io/google_containers/pause-amd64:3.0"
+"Windows Server 2016" )
+echo -n "Cache Images"
+for image in ${StringArray[@]}; do
+    docker pull ${image} 1>/dev/null
+    echo -n "- ${image}"
+done
+
+# Wait for Kubernetes to be up
+echo -n "Waiting for Kubernetes to be ready to use" &&
+until kubectl get pod 2>/dev/null ; do echo -n . ; sleep 1 ; done ; echo &&
+echo "Ready to go!"
